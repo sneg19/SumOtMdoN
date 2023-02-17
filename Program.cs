6f89d3503335d062aed23c8e28750197c27e8781
sneg <sneg.sbg@gmail.com>
2023-02-17 14:50:44 +0300

    working programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int PrintSum(int m, int n)
+{
+    if(n==m) return n;
+    else return n + PrintSum(m, n-1); 
+}
+
+void Main()
+{
+    Console.Write("Введите чсло M: ");
+    int m = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите чсло N: ");
+    int n = int.Parse(Console.ReadLine()!);
+    Console.WriteLine($" N = {n}; M = {m} -> {PrintSum(m, n)}");
+}
+
+Main();
